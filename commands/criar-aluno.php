2c596a4cc58fd9c09ea1813fbc4557ecfe48034e
douglasbernardo <43753293+douglasbernardo@users.noreply.github.com>
2020-03-06 21:09:46 -0300

    adicionar aluno

--- commands/criar-aluno.php
+++ commands/criar-aluno.php
@@ -6,7 +6,8 @@ use Douglas\Doctrine\Helper\EntityManagerFactory;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $aluno = new Aluno();
-$aluno->setNome("$argv[1]");
+$aluno->setNome($argv[1]);
+$aluno->setIdade($argv[2]);
 
 $entityManagerFactory = new EntityManagerFactory();
 $entityManager = $entityManagerFactory->getEntityManager();
