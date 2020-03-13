34425f8ca846df719c920ad94380b9567369125b
douglasbernardo <43753293+douglasbernardo@users.noreply.github.com>
2020-03-13 16:28:07 -0300

    criando entidade telefone

--- commands/criar-aluno.php
+++ commands/criar-aluno.php
@@ -1,16 +1,26 @@
 <?php
 
 use Douglas\Doctrine\Entity\Aluno;
+use Douglas\Doctrine\Entity\Telefone;
 use Douglas\Doctrine\Helper\EntityManagerFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+$entityManagerFactory = new EntityManagerFactory();
+$entityManager = $entityManagerFactory->getEntityManager();
 
 $aluno = new Aluno();
 $aluno->setNome($argv[1]);
 $aluno->setIdade($argv[2]);
 
-$entityManagerFactory = new EntityManagerFactory();
-$entityManager = $entityManagerFactory->getEntityManager();
+for($i = 3; $i < $argc;$i++){
+    $numeroTelefone = $argv[$i];
+    $telefone = new Telefone();
+    $telefone->setNumero($numeroTelefone);
+
+    $entityManager->persist($telefone);
+    $aluno->addTelefone($telefone);
+}
 
 $entityManager->persist($aluno);
 
